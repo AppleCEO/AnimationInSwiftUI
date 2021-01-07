f3956fa7d52619e843748e2b1fdc45cf02c8b21d
dominic <kiljh@me.com>
2021-01-07 14:16:51 +0900

    커스텀 뷰 애니메이션

--- animation/ContentView.swift
+++ animation/ContentView.swift
@@ -8,24 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showText: Bool = false
+    @State private var trimmedTo: CGFloat = 1.0
+    @State private var lineWidth: CGFloat = 7.0
+    @State private var isHidden = false
     
     var body: some View {
         VStack {
-            if showText {
-                Text("Transition")
-                    .font(.largeTitle)
-                    .padding()
-                    .transition(.modifier(active: CustomScaleModifier(scale: 0), identity: CustomScaleModifier(scale: 1)))
-            }
-        
-        Button("Display Text On / Off") {
-            withAnimation {
-                self.showText.toggle()
-            }
-        }.font(.title)
+            MyCircle(trimmedTo: trimmedTo, lineWidth: lineWidth)
+                .padding()
+                .animation(.easeInOut(duration: 1.5))
+            Button(action: {
+                self.trimmedTo = self.isHidden ? 1.0 : 0
+                self.lineWidth = self.isHidden ? 7 : 1
+                self.isHidden.toggle()
+            }) {
+                Text("Animate!").font(.title)
+            }.padding()
+        }
     }
-}
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -34,10 +34,31 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct CustomScaleModifier: ViewModifier {
-    let scale: CGFloat
-    func body(content: Content) -> some View {
-        content
-            .scaleEffect(scale)
+struct MyCircle: Shape {
+    var trimmedTo: CGFloat
+    var lineWidth: CGFloat
+    var animatableData: AnimatablePair<CGFloat, CGFloat> {
+        get {
+            AnimatablePair(trimmedTo, lineWidth)
+        }
+        set {
+            trimmedTo = newValue.first
+            lineWidth = newValue.second
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = rect.width / 2
+        let start = Angle(radians: .pi / -2)
+        let end = Angle(radians: .pi / 2 * 3)
+        
+        path.addArc(center: center, radius: radius / 4, startAngle: start, endAngle: end, clockwise: false)
+        path.addArc(center: center, radius: radius / 2, startAngle: start, endAngle: end, clockwise: false)
+        path.addArc(center: center, radius: radius, startAngle: start, endAngle: end, clockwise: false)
+        return path
+            .trimmedPath(from: 0.0, to: trimmedTo)
+            .strokedPath(.init(lineWidth: lineWidth))
     }
 }
